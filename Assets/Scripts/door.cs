5b62037e4133d58cbb439a436cbdd9994fc26e5c
Ilucik <62407205+Ilucik@users.noreply.github.com>
2021-07-15 19:55:10 +0500

    прикрутил механику рычага к первой двери, добавил текстовое поле

---
+++ Assets/Scripts/door.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class door : MonoBehaviour
+{
+    public void MoveDown()
+    {
+        StartCoroutine(MoveObject(transform.position, transform.position + new Vector3(0,-2.5f,0)));
+    }
+
+    private IEnumerator MoveObject(Vector3 origin, Vector3 destination)
+    {
+        float totalMovementTime = 5f;
+        float currentMovementTime = 0f;
+        while (Vector3.Distance(transform.position, destination) > 0)
+        {
+            currentMovementTime += Time.deltaTime;
+            transform.position = Vector3.Lerp(origin, destination, currentMovementTime / totalMovementTime);
+            yield return null;
+        }
+    }   
+}
