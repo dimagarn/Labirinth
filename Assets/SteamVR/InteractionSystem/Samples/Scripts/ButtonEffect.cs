76071e03c52ffb0ebd74e7f5ffe14acc7637e454
Ilucik <62407205+Ilucik@users.noreply.github.com>
2021-07-16 15:44:59 +0500

    /

--- Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
+++ Assets/SteamVR/InteractionSystem/Samples/Scripts/ButtonEffect.cs
@@ -9,19 +9,23 @@ namespace Valve.VR.InteractionSystem.Sample
 {
     public class ButtonEffect : MonoBehaviour
     {
-        public string Scene;
-        public GameObject men;
+        public GameObject point;
+        public string tag;
         private bool isEnable = false;
         public void OnButtonDown(Hand fromHand)
         {
             ColorSelf(Color.cyan);
             fromHand.TriggerHapticPulse(1000);
+
             Debug.Log("Button");
             Vector3 oldPosition = Player.instance.transform.position;
             if (isEnable)
             {
-                Destroy(men);
-                SceneManager.LoadScene(Scene);
+                oldPosition = transform.position;
+                if (this.tag == "samplButton")
+                    transform.position = point.transform.position;
+                else
+                    transform.position = oldPosition;
             }
         }
 
